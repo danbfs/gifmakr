927807925aa847f9a438d3f4994cd933a229e787
danbfs <danielbaldifs@gmail.com>
2022-02-20 14:37:41 -0300

    Initial commit (w/ node_modules and yarn.lock)

---
+++ snowpack.config.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+
+module.exports = {
+  devOptions: {
+    responseHeaders: {
+      "Cross-Origin-Embedder-Policy": "require-corp",
+      "Cross-Origin-Opener-Policy": "same-origin",
+    },
+  },
+  routes: [
+    {
+      match: "routes",
+      src: ".*",
+      dest: (req, res) => {
+        // Default headers. See https://github.com/withastro/snowpack/blob/c44d86f73ac7b74507d4d5554ccb96e8b7dc5294/snowpack/src/commands/dev.ts#L133-L137.
+        res.setHeader("Accept-Ranges", "bytes");
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Content-Type", "text/html");
+        res.setHeader("Vary", "Accept-Encoding");
+
+        // Make `index.html` cross-origin isolated to enable `SharedArrayBuffer`.
+        res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+        res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+
+        return fs.createReadStream("index.html").pipe(res);
+      },
+    },
+  ],
+};
